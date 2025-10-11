b4881f2212bb4003f2936a65836635427c545347
ekoret <ekoret123@gmail.com>
2025-10-11 00:55:18 -0700

    ui: add displaying dummb data to chat window

--- src/components/Chat/ChatWindow.tsx
+++ src/components/Chat/ChatWindow.tsx
@@ -1,5 +1,22 @@
-const ChatWindow = () => {
-  return <div className="flex-1">chat window</div>;
+import type { IChatMessage } from "../../pages/Chat";
+
+interface ChatWindowProps {
+  chatMessages: IChatMessage[];
+}
+
+const ChatWindow = ({ chatMessages }: ChatWindowProps) => {
+  return (
+    <div className="flex-1 overflow-y-auto mb-4">
+      {chatMessages.map((message) => {
+        return (
+          <div className="mb-4">
+            <h3 className="font-bold">{message.sender} says:</h3>
+            <p>{message.message}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ChatWindow;
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,12 +1,92 @@
+import { useState } from "react";
 import ChatControls from "../components/Chat/ChatControls";
 import ChatSettings from "../components/Chat/ChatSettings";
 import ChatWindow from "../components/Chat/ChatWindow";
 
+export interface IChatMessage {
+  sender: string;
+  message: string;
+}
+
 function Chat() {
+  const initMessages: IChatMessage[] = [
+    {
+      sender: "ekoret",
+      message: "hi",
+    },
+    {
+      sender: "ekoret",
+      message: "how are you?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your name?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite color?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite food?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite movie?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite book?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite sport?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite game?",
+    },
+    {
+      sender: "ekoret",
+      message: "how are you?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your name?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite color?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite food?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite movie?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite book?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite sport?",
+    },
+    {
+      sender: "ekoret",
+      message: "what is your favorite game?",
+    },
+  ];
+
+  const [chatMessages, setChatMessages] =
+    useState<IChatMessage[]>(initMessages);
+
   return (
     <div className="h-full flex flex-col">
       <ChatSettings />
-      <ChatWindow />
+      <ChatWindow chatMessages={chatMessages} />
       <ChatControls />
     </div>
   );
