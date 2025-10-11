5d008f1b39ba65e6e3305e2efc77e48e2685848f
ekoret <ekoret123@gmail.com>
2025-10-11 00:27:27 -0700

    feature: user details in sidebar

---
+++ src/UserDetails.tsx
@@ -0,0 +1,17 @@
+import imgUrl from "../public/neo-banana-cat.jpg";
+
+function UserDetails() {
+  return (
+    <div className="bg-user-details-bg rounded py-2 px-4 flex gap-4">
+      <div>
+        <img className="rounded-full max-w-[50px]" src={imgUrl} />
+      </div>
+      <div>
+        <small>Logged in as</small>
+        <h3>ekoret</h3>
+      </div>
+    </div>
+  );
+}
+
+export default UserDetails;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,17 @@
+import UserDetails from "../UserDetails";
 import ThemeToggle from "./ThemeToggle";
 
 function Sidebar() {
   return (
-    <aside className="bg-sidebar flex flex-col gap-2 min-w-[170px] max-w[300px]">
+    <aside className="bg-sidebar flex flex-col gap-2 min-w-[250px] max-w[300px]">
       <h1 className="mb-8 text-4xl font-bold">SocketChat</h1>
       <nav className="flex flex-col gap-4">
         <a href="/">Dashboard</a>
         <a href="/settings">Settings</a>
       </nav>
-      <div className="mt-auto">
+      <div className="mt-auto flex flex-col gap-4">
         <ThemeToggle />
+        <UserDetails />
       </div>
     </aside>
   );
