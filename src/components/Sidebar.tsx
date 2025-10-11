c2687df40c76dfcb89ae9bd2666206a1bd6d3370
ekoret <ekoret123@gmail.com>
2025-10-11 16:51:25 -0700

    refactor: remove old layout component in favour of tanstack root

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,19 @@
 import UserDetails from "./UserDetails";
 import ThemeToggle from "./ThemeToggle";
+import ConnectionButtons from "./ConnectionButtons";
+import { Link } from "@tanstack/react-router";
 
 function Sidebar() {
   return (
     <aside className="bg-sidebar flex flex-col gap-2 min-w-[250px] max-w[300px]">
       <h1 className="mt-20 mb-8 text-4xl font-bold">SocketChat</h1>
       <nav className="flex flex-col gap-4">
-        <a href="/">Dashboard</a>
-        <a href="/settings">Settings</a>
+        <Link to="/">Dashboard</Link>
+        <Link to="/chat">Chat</Link>
+        <Link to="/settings">Settings</Link>
       </nav>
       <div className="mt-auto flex flex-col gap-4">
+        <ConnectionButtons />
         <ThemeToggle />
         <UserDetails />
       </div>
