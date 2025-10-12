fb9bf300586e70f93ebd04cde5467b6fc3565b0e
ekoret <ekoret123@gmail.com>
2025-10-12 02:02:17 -0700

    feature: add some random charts to dashboard to fill space

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
+import VerticalBarChart from "../components/Charts/VerticalBarChart";
+import PieChart from "../components/Charts/PieChart";
+import HorizontalBarChart from "../components/Charts/HorizontalBarChart";
+import ScatterChart from "../components/Charts/ScatterChart";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return (
+    <>
+      <VerticalBarChart />
+      <PieChart />
+      <HorizontalBarChart />
+      <ScatterChart />
+    </>
+  );
 }
