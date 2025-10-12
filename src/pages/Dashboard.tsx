ecb96e7bb2ffc44646cef06b5389ba4d369755de
ekoret <ekoret123@gmail.com>
2025-10-12 16:22:18 -0700

    ui: add simple dashboard ui

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,38 @@
+import { useUserContext } from "../hooks/useUserContext";
+
 function Dashboard() {
+  const userContext = useUserContext();
+
+  /**
+   * TODO: is null assertion safe here?
+   *
+   * The router will never navigate to this page if
+   * the user is null due to the router guard.
+   */
+  const user = userContext.user!;
+
   return (
     <div className="h-full overflow-y-auto">
       <div className="flex-col gap-4 mt-52">
-        <h2 className="text-4xl">Welcome</h2>
+        <div className="mb-8">
+          <h2 className="text-3xl mb-4">
+            Welcome,<br></br>
+            <span className="text-4xl">{user.username}</span>
+          </h2>
+          <p>You're all set. Jump into the chat!</p>
+        </div>
+        <div className="mb-4">
+          <small>You logged in at</small>
+
+          <h3 className="font-bold">
+            {new Date(user.connectedAt).toLocaleTimeString()}
+          </h3>
+        </div>
+        <div className="mb-4">
+          <small>Connection Status</small>
+
+          <h3 className="font-bold">{user.connected ? "Online" : "Offline"}</h3>
+        </div>
       </div>
     </div>
   );
