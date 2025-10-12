667e14678aa73fa654b2312bb0a1d2a4c97ec552
ekoret <ekoret123@gmail.com>
2025-10-12 01:33:10 -0700

    feature: add logout button

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,10 +1,25 @@
 import { useContext } from "react";
 import imgUrl from "../assets/neo-banana-cat.jpg";
 import { AppContext } from "../contexts/AppContext";
+import AppIcon from "./AppIcon";
+import { LogOut } from "lucide-react";
+import { useNavigate } from "@tanstack/react-router";
 
 function UserDetails() {
+  const navigate = useNavigate();
   const context = useContext(AppContext);
   const user = context?.globalData.user;
+
+  const handleLogout = () => {
+    context?.setGlobalData((prev) => ({
+      ...prev,
+      user: undefined,
+    }));
+
+    navigate({
+      to: "/login",
+    });
+  };
 
   return (
     <div className="bg-user-details-bg rounded py-2 px-4 flex gap-4">
@@ -14,6 +29,13 @@ function UserDetails() {
       <div>
         <small>Logged in as</small>
         <h3>{user?.username}</h3>
+      </div>
+      <div
+        onClick={handleLogout}
+        title="Logout"
+        className="ml-auto cursor-pointer"
+      >
+        <AppIcon Icon={LogOut} />
       </div>
     </div>
   );
